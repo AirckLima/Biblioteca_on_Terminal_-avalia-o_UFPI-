26d9843bc94bdef232226ad5f0d618765ecb0cae
Manoel Rocha <manoelmelo367@gmail.com>
2023-02-10 12:14:30 -0300

    Funçao adicionar usuários

--- usuarios.py
+++ usuarios.py
@@ -1,16 +1,18 @@
 caminho_usuarios = 'usuarios.txt'
 
-def adicionarUsuario (nome, email, senha,):
+def adicionarUsuario (nome, email, senha, livros):
     with open(caminho_usuarios, 'a+') as usuario:
-     usuario.write(f'nome: {nome}\n')
-     usuario.write(f'email: {email}\n')
-     usuario.write(f'Senha: {senha}\n')
+
+     usuario.write(f'nome: {nome}, ')
+     usuario.write(f'email: {email}, ')
+     usuario.write(f'Senha: {senha}, ')
+     usuario.write(f'Livros: {livros}')
 
 nome = input("Digite seu nome:\n")
 email = input("Digite seu email:\n")
 senha = input("Crie sua senha:\n")
 
-adicionarUsuario(nome, email, senha)
+adicionarUsuario(nome, email, senha, "Pequeno principe")
 
 def alterarUsuario():
     print('Usuario alterado')
