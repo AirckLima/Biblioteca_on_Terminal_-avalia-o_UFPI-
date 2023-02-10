c889fde07eb640f65e35f1a2d6b25c07e2a332bd
Airck Lima <aircklima@gmail.com>
2023-02-09 21:54:53 -0300

    emprestimo v1

--- emprestimos/emprestimos.py
+++ emprestimos/emprestimos.py
@@ -2,11 +2,16 @@ def emprestarlivro():
     nome=input('qual o seu nome?')
     cadrastro=input('qual o seu cadrastro?')
     nomedolivro=input('qual o nome do livro?')
-    usuario={"nome": "batista", "cadastro": 12345, "listadelivros": ["hobbit", "harry potter", "sherlock", "abc"]}
+
+    listadelivros = ["hobbit", "harry potter", "sherlock", "abc"]
+
+    usuario={"nome": nome, "cadastro": int(cadrastro), "listadelivros": listadelivros}
+    
     if len(usuario['listadelivros']) > 3:
         print('voce nao pode pegar mais um livro')
-    print(len(usuario['listadelivros']))
+    
 
 
 
-emprestarlivro()
+if __name__ == "__main__":
+    emprestarlivro()
